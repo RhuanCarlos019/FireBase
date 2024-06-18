e362aefbc8fcc0962d5b6a17f18c6ae3942a06d2
RhuanCarlos019 <rhuan.estudos019@gmail.com>
2024-06-18 10:20:09 -0300

    ok

---
+++ projeto_firebase/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:projeto_firebase/firebase_options.dart';
+import 'package:projeto_firebase/screens/home_screen.dart';
+
+import 'screens/login_screen.dart';
+import 'screens/registro_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Teste Firebase App",
+      home:const HomeScreen(),
+      routes: {
+        '/login':(context) => const LoginScreen(),
+        '/registro':(context) => const RegistroScreen()
+      } ,
+    );
+  }
+}
